ebb5843e77b83b49474d844a22475d997552b49e
Simon Lynch <simon.lynch@hashicorp.com>
2025-10-30 05:34:15 +0800

    feat: Add EKS add-ons configuration and outputs for Argo Workflows and AWS Load Balancer Controller

---
+++ aws-eks-addon/providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  
+required_providers {
+ 
+  kubernetes = {
+    source  = "hashicorp/kubernetes"
+    version = "~> 2.25"
+  }
+
+  time = {
+    source = "hashicorp/time"
+    version = "~> 0.1"
+  }
+  
+
+  helm = {
+    source = "hashicorp/helm"
+    version = "~> 2.1"
+  }
+
+  random = {
+    source = "hashicorp/random"
+    version = "~> 3.7.0"
+  }
+
+
+}
+}
